6ee700221e4549ce1a729a01a1b115528a4ba161
Marvin Bernd <marvinbernd@Marvins-MacBook-Pro.local>
2020-05-01 18:40:07 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/mb-favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
